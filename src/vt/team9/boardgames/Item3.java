ab22d097e1d6a0380f5be4128ae644e91595c367
Adil Mahmood <adil.m@aol.com>
2013-11-19 03:20:01 -0500

    UI update.

---
+++ src/vt/team9/boardgames/Item3.java
@@ -0,0 +1,16 @@
+package vt.team9.boardgames;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.actionbarsherlock.app.SherlockFragment;
+import vt.team9.boardgame.R;
+
+public class Item3 extends SherlockFragment{
+
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+		return inflater.inflate(R.layout.item3, null);
+	}
+}
